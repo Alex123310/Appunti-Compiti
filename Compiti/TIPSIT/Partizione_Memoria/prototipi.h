cc69e8ed91a85ebcec5db694d41eac5e53ee581a
Alex123310 <alessandrett@gmail.com>
2024-01-30 17:20:26 +0100

    Changes to be committed: 	new file:   Compiti/TIPSIT/Partizione_Memoria/Alloca_Processo.c 	new file:   Compiti/TIPSIT/Partizione_Memoria/Alloca_Processo.o 	new file:   Compiti/TIPSIT/Partizione_Memoria/Crea_Partizione.c 	new file:   Compiti/TIPSIT/Partizione_Memoria/Crea_Partizione.o 	new file:   Compiti/TIPSIT/Partizione_Memoria/Makefile.win 	new file:   Compiti/TIPSIT/Partizione_Memoria/Partizione_Memoria.7z 	new file:   Compiti/TIPSIT/Partizione_Memoria/Partizione_Memoria.dev 	new file:   Compiti/TIPSIT/Partizione_Memoria/Partizione_Memoria.exe 	new file:   Compiti/TIPSIT/Partizione_Memoria/Partizione_Memoria.layout 	new file:   Compiti/TIPSIT/Partizione_Memoria/Stampa_Partizione.c 	new file:   Compiti/TIPSIT/Partizione_Memoria/Stampa_Partizione.o 	new file:   Compiti/TIPSIT/Partizione_Memoria/costanti.h 	new file:   Compiti/TIPSIT/Partizione_Memoria/main.c 	new file:   Compiti/TIPSIT/Partizione_Memoria/main.o 	new file:   Compiti/TIPSIT/Partizione_Memoria/prototipi.h 	new file:   Compiti/TIPSIT/Partizione_Memoria/tipi.h

---
+++ Compiti/TIPSIT/Partizione_Memoria/prototipi.h
@@ -0,0 +1,6 @@
+//CREA LA PARTIZIONE
+void CreatePartition(Partizione partizioni[]); 
+//ALLOCA IL PROCESSO
+int Alloca_Processo(Partizione part[], int DimensioneProesso); 
+//STAMPA LA PARTIZIONE A SCHERMO
+void VisualizzaPartizione(Partizione part[]);
