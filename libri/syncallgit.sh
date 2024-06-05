8e4c73c925094e49aa35ed42c5ad97670fe92692
Alex123310 <alessandrett@gmail.com>
2024-06-05 12:21:17 +0200

    ULTIMO COMMIR DELL ANNO FIGLI DI PUTTANA

---
+++ libri/syncallgit.sh
@@ -0,0 +1,33 @@
+
+
+
+
+#----------------------------------------------------
+
+cd ~/Desktop/ &
+
+cd Appunti-Compiti/
+git pull
+
+cd ..
+
+cd CompitiChierego/
+git pull
+
+
+
+#------------------------COMMIT----------------------
+cd ~/Desktop/
+cd Appunti-Compiti/
+git add .
+git commit -m "Auto Commit"
+git push
+cd ..
+
+
+cd CompitiChierego/
+git add .
+git commit -m "Auto Commit"
+git push
+cd ..
+
